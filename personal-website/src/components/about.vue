507b8f1fd21e1df24feed5f1a2923882c9cfd0ee
George Sarantinos <georgesarantinos18@gmail.com>
2021-02-04 22:14:07 -0800

    improving collage

--- personal-website/src/components/about.vue
+++ personal-website/src/components/about.vue
@@ -1,30 +1,50 @@
 <template>
   <div>
     <div class="aboutMe">
-      <h2 class="label">About Me</h2>
+      <div class="aboutLabelWrapper">
+        <h2 class="aboutLabel">About Me</h2>
+      </div>
       <p>
-        'My time at Red Bull Media House has been incredible. Being able to work with' +
-        ' a group of experienced, driven individuals who show passion for what they do ' +
-        'both in and out of the office inspires me on a daily basis.',
-        'After proving my abilities to the team leadership, I was promoted from a junior' +
-        ' Software Engineer to Software Engineer at the beginning of 2019. Over the past ' +
-        'two plus years I’ve worked on the backend engineering team, building and ' +
-        'maintaining the services that power the Red Bull TV and Servus TV streaming ' +
-        'services and Red Bull AR App.',
-        'During this time I have gained significant experience in video streaming. A ' +
-        'unique project that my team and I built was our multilinear service, which ' +
-        'generates 24/7 HLS and DASH linear streams and accompanying electronic ' +
-        'programming guides. By doing so, Red Bull TV now offers multiple linear stream ' +
-        'channels along with VOD and live streams. Along with this, I helped build the ' +
-        'metadata and stream localization features for Red Bull TV, improving the ' +
-        'content served to numerous regions.',
-        'Want to learn more about what I’ve worked on at Red Bull Media House (ETL' +
-        ' systems, leaderboard services, data migrations, and more!)? Connect with me ' +
-        'on LinkedIn! Let’s chat.'
+        When I look back on how I landed in the field of software engineering, I often reflect
+        on the things I've always enjoyed doing. Getting in a good day at the mountain skiing,
+        learning a new song on the guitar, or documenting random events of my life through short
+        form videos are all activities that I grew up loving to do.
+        <br><br>
+        I find that I am attracted to the challenging nature of these things. There is
+        persistence needed to build my skill and problem solving is always involved. What's my
+        best line down a difficult ski run? How much longer do I need to practice to perfect this
+        song? How should I edit this part of the video to keep the viewer entertained?
+        <br><br>
+        Although as a software engineer I ask different questions, my mindset is in a similar state.
+        Software engineering is challenging, takes persistence, and at it's core is problem solving.
+        This is what I love about the work I do.
+        <br><br>
+        But let's not be too analytical here! Outside of the office you'll find me enjoying the
+        outdoors, whether that's exploring the city I'm in or heading out for a weekend in the
+        mountains. More often than not music is involved. That usually means a jam session on
+        the piano or guitar, or possibly blasting some good old Seattle grunge in the car. But
+        in the end, what makes all of these things better is when I get to do them with my friends
+        and family! They're the people that keep me going, keep me laughing, and keep me loving
+        life.
+
       </p>
     </div>
     <div class="collage">
-      <img class="portrait" src="https://res.cloudinary.com/df1dpirbp/image/upload/w_350,h_200/v1609570109/LMU1_t6sf4w.png" alt=""/>
+      <div class="imageRow">
+        <img class="long" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1612501041/book_zpjxm6.heic" alt=""/>
+        <img class="tall" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1611365602/walk_gjbscw.jpg" alt=""/>
+        <img class="long" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1611365590/wine_kwzdsl.jpg" alt=""/>
+      </div>
+      <div class="imageRow">
+        <img class="tall" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1611365609/look_evj1cv.jpg" alt=""/>
+        <img class="long" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1611365569/ski_sh2b91.jpg" alt=""/>
+        <img class="long" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1612501030/henry_tsler1.jpg" alt=""/>
+      </div>
+      <div class="imageRow">
+        <img class="long" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1612501041/book_zpjxm6.heic" alt=""/>
+        <img class="tall" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1611365602/walk_gjbscw.jpg" alt=""/>
+        <img class="long" src="https://res.cloudinary.com/df1dpirbp/image/upload/q_auto,f_auto/v1611365590/wine_kwzdsl.jpg" alt=""/>
+      </div>
     </div>
   </div>
 </template>
@@ -33,11 +53,6 @@
   export default {
     name: 'about',
     components: {
-    },
-    data: () => {
-      return {
-        fixed: false
-      }
     }
   }
 </script>
@@ -45,35 +60,35 @@
 <style>
   .aboutMe {
     float: right;
-    width: 45%;
+    width: 650px;
     text-align: center;
     text-autospace: initial;
     line-height: 1.5;
-    padding: 30px 5vw;
-    background-color: mintcream;
-    height: 600px;
+    padding: 30px 65px;
+    background-color: #DAAD86;
+    border-left: 5px solid #659DBD;
+    height: 770px;
+    position: relative;
   }
 
   .collage {
-    float: left;
-    background-color: blue;
-    height: 600px;
-    width: 55%;
+    background-color: #659DBD;
+    height: 770px;
+    width: 100vw;
   }
 
-  .label {
+  .aboutLabel {
     overflow: hidden;
     white-space: nowrap;
     text-align: center;
     margin-right: 25%;
     margin-left: 25%;
     margin-bottom: 20px;
-    position: sticky;
     top: 0;
   }
 
-  .label:before,
-  .label:after {
+  .aboutLabel:before,
+  .aboutLabel:after {
     background-color: #000;
     content: "";
     display: inline-block;
@@ -83,19 +98,44 @@
     width: 50%;
   }
 
-  .label:before {
+  .aboutLabel:before {
     right: 0.5em;
     margin-left: -50%;
   }
 
-  .label:after {
+  .aboutLabel:after {
     left: 0.5em;
     margin-right: -50%;
   }
 
-  .portrait {
+  .aboutLabelWrapper {
+    top: 0;
+    bottom: -200px;
+    padding-top: 25px;
     position: sticky;
-    margin-top: 100px;
-    top: 200px;
+    background-color: #DAAD86;
+    padding-bottom: 3px;
+  }
+
+  .imageRow {
+    display: flex;
+    align-items: center;
+    z-index: -1;
+  }
+
+  .long {
+    height: 250px;
+    width: 333px;
+    float: left;
+    margin-left: 5px;
+    margin-top: 5px;
+  }
+
+  .tall {
+    height: 250px;
+    width: 188px;
+    float: left;
+    margin-left: 5px;
+    margin-top: 5px;
   }
 </style>
