a4b55f2e01e067772f0bb0dee5a8404240a29ed6
George Sarantinos <georgesarantinos18@gmail.com>
2021-02-11 18:59:01 -0800

    cleaning up code

--- personal-website/src/components/experienceItem.vue
+++ personal-website/src/components/experienceItem.vue
@@ -16,7 +16,7 @@
             {{ paragraph }}<br><br>
           </p>
         </div>
-        <p :class="{noDots: isExpanded, dots: !isExpanded}">...</p>
+        <p v-if="!isExpanded" class="dots">...</p>
         <div v-on:click="expandText" :class="{expandButtonUp: isExpanded, expandButton: !isExpanded}">
           <i class="smallArrow smallDown" v-if="!isExpanded"></i>
           <i class="smallArrow smallUp" v-if="isExpanded"></i>
@@ -27,8 +27,6 @@
 </template>
 
 <script>
-  import _ from "lodash";
-
   export default {
     name: 'experienceItem',
     props: {
@@ -75,6 +73,226 @@
     font-style: italic;
     font-weight: bold;
     margin-bottom: 5px;
+  }
+
+  .collapsed {
+    max-height: 130px;
+    overflow: hidden;
+    animation: collapseText 1s;
+    -webkit-animation: collapseText 1s;
+    -moz-animation: collapseText 1s;
+    -o-animation: collapseText 1s;
+    -ms-animation: collapseText 1s;
+    animation-fill-mode: forwards;
+    -webkit-animation-fill-mode: forwards;
+    -moz-animation-fill-mode: forwards;
+    -o-animation-fill-mode: forwards;
+    -ms-animation-fill-mode: forwards;
+  }
+
+  .expanded {
+    animation: expandText 1s;
+    -webkit-animation: expandText 1s;
+    -moz-animation: expandText 1s;
+    -o-animation: expandText 1s;
+    -ms-animation: expandText 1s;
+    animation-fill-mode: forwards;
+    -webkit-animation-fill-mode: forwards;
+    -moz-animation-fill-mode: forwards;
+    -o-animation-fill-mode: forwards;
+    -ms-animation-fill-mode: forwards;
+  }
+
+  .dots {
+    text-align: center;
+    font-weight: bold;
+  }
+
+  .expandButton {
+    position: relative;
+    align-items: center;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    cursor: pointer;
+    margin: auto;
+    background-color: rgba(192,192,192, 0.4);
+  }
+
+  .expandButtonUp {
+    position: relative;
+    align-items: center;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    cursor: pointer;
+    margin: auto;
+    background-color: rgba(192,192,192, 0.4);
+  }
+
+  .smallArrow {
+    border: solid black;
+    border-width: 0 2px 2px 0;
+    padding: 4px;
+    position: absolute;
+    display: table-cell;
+    margin-left: 5px;
+    margin-top: 3px;
+  }
+
+  .smallDown {
+    animation: reverseSpin .5s;
+    -webkit-animation: reverseSpin .5s;
+    -moz-animation: reverseSpin .5s;
+    -o-animation: reverseSpin .5s;
+    -ms-animation: reverseSpin .5s;
+    animation-fill-mode: forwards;
+    -webkit-animation-fill-mode: forwards;
+    -moz-animation-fill-mode: forwards;
+    -o-animation-fill-mode: forwards;
+    -ms-animation-fill-mode: forwards;
+  }
+
+  .smallUp {
+    animation: spin .5s;
+    -webkit-animation: spin .5s;
+    -moz-animation: spin .5s;
+    -o-animation: spin .5s;
+    -ms-animation: spin .5s;
+    animation-fill-mode: forwards;
+    -webkit-animation-fill-mode: forwards;
+    -moz-animation-fill-mode: forwards;
+    -o-animation-fill-mode: forwards;
+    -ms-animation-fill-mode: forwards;
+  }
+
+  @media (min-width: 1000px) {
+    .itemExpanded {
+      margin-bottom: 30px;
+    }
+
+    .itemCollapsed {
+      margin-bottom: 70px;
+    }
+
+    .imageHolderLeft {
+      float: left;
+      position: relative;
+      margin-left: 5vw;
+      margin-right: 20px;
+      border-radius: 25px;
+      width: 400px;
+      height: 250px;
+      background-color: rgba(192,192,192, 0.4);
+      background-repeat: no-repeat;
+      background-blend-mode: screen;
+    }
+
+    .imageHolderRight {
+      float: right;
+      position: relative;
+      margin-right: 5vw;
+      margin-left: 20px;
+      border-radius: 25px;
+      width: 400px;
+      height: 250px;
+      background-color: rgba(192,192,192, 0.4);
+      background-repeat: no-repeat;
+      background-blend-mode: screen;
+    }
+
+    .infoLeft {
+      margin-right: 5vw;
+      overflow: hidden;
+      text-align: left;
+    }
+
+    .infoRight {
+      margin-left: 5vw;
+      overflow: hidden;
+      text-align: left;
+    }
+  }
+
+  @media (max-width: 1000px) {
+    .itemExpanded {
+      margin-bottom: 10px;
+    }
+
+    .itemCollapsed {
+      margin-bottom: 40px;
+    }
+
+    .imageHolderLeft {
+      position: relative;
+      margin-left: auto;
+      margin-right: auto;
+      border-radius: 25px;
+      width: 400px;
+      height: 250px;
+      background-color: rgba(192,192,192, 0.4);
+      background-repeat: no-repeat;
+      background-blend-mode: screen;
+
+    }
+
+    .imageHolderRight {
+      position: relative;
+      margin-left: auto;
+      margin-right: auto;
+      border-radius: 25px;
+      width: 400px;
+      height: 250px;
+      background-color: rgba(192,192,192, 0.4);
+      background-repeat: no-repeat;
+      background-blend-mode: screen;
+    }
+
+    .infoLeft {
+      margin-right: 10vw;
+      margin-left: 10vw;
+      margin-top: 20px;
+      overflow: hidden;
+      text-align: center;
+    }
+
+    .infoRight {
+      margin-left: 10vw;
+      margin-right: 10vw;
+      margin-top: 20px;
+      overflow: hidden;
+      text-align: center;
+    }
+
+    .expandButtonUp {
+      margin-bottom: 25px;
+    }
+  }
+
+  @media (max-width: 500px) {
+    .imageHolderLeft {
+      width: 350px;
+      height: 219px;
+    }
+
+    .imageHolderRight {
+      width: 350px;
+      height: 219px;
+    }
+
+    .image {
+      transform: scale(.85, .85) translate(-50%, -50%);
+      left: 42.5%;
+      top: 42.5%;
+    }
+
+    .expanded {
+      margin-bottom: 70px;
+    }
+
+    .expandButtonUp {
+      margin-top: -15vw;
+    }
   }
 
   @keyframes expandText {
@@ -165,74 +383,6 @@
     100% {
       max-height: 130px;
     }
-  }
-
-  .collapsed {
-    max-height: 130px;
-    overflow: hidden;
-    animation: collapseText 1s;
-    -webkit-animation: collapseText 1s;
-    -moz-animation: collapseText 1s;
-    -o-animation: collapseText 1s;
-    -ms-animation: collapseText 1s;
-    animation-fill-mode: forwards;
-    -webkit-animation-fill-mode: forwards;
-    -moz-animation-fill-mode: forwards;
-    -o-animation-fill-mode: forwards;
-    -ms-animation-fill-mode: forwards;
-  }
-
-  .expanded {
-    animation: expandText 1s;
-    -webkit-animation: expandText 1s;
-    -moz-animation: expandText 1s;
-    -o-animation: expandText 1s;
-    -ms-animation: expandText 1s;
-    animation-fill-mode: forwards;
-    -webkit-animation-fill-mode: forwards;
-    -moz-animation-fill-mode: forwards;
-    -o-animation-fill-mode: forwards;
-    -ms-animation-fill-mode: forwards;
-  }
-
-  .dots {
-    text-align: center;
-    font-weight: bold;
-  }
-
-  .noDots {
-    visibility: hidden;
-  }
-
-  .expandButton {
-    align-items: center;
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    cursor: pointer;
-    margin: 1px auto auto;
-    background-color: rgba(192,192,192, 0.4);
-  }
-
-  .expandButtonUp {
-    position: relative;
-    align-items: center;
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    cursor: pointer;
-    margin: auto;
-    background-color: rgba(192,192,192, 0.4);
-  }
-
-  .smallArrow {
-    border: solid black;
-    border-width: 0 2px 2px 0;
-    padding: 4px;
-    position: absolute;
-    display: table-cell;
-    margin-left: 5px;
-    margin-top: 3px;
   }
 
   @keyframes spin {
@@ -344,162 +494,6 @@
     100% {
       -ms-transform: rotate(45deg);
       margin-top: 3px;
-    }
-  }
-
-  .smallDown {
-    animation: reverseSpin .5s;
-    -webkit-animation: reverseSpin .5s;
-    -moz-animation: reverseSpin .5s;
-    -o-animation: reverseSpin .5s;
-    -ms-animation: reverseSpin .5s;
-    animation-fill-mode: forwards;
-    -webkit-animation-fill-mode: forwards;
-    -moz-animation-fill-mode: forwards;
-    -o-animation-fill-mode: forwards;
-    -ms-animation-fill-mode: forwards;
-  }
-
-  .smallUp {
-    animation: spin .5s;
-    -webkit-animation: spin .5s;
-    -moz-animation: spin .5s;
-    -o-animation: spin .5s;
-    -ms-animation: spin .5s;
-    animation-fill-mode: forwards;
-    -webkit-animation-fill-mode: forwards;
-    -moz-animation-fill-mode: forwards;
-    -o-animation-fill-mode: forwards;
-    -ms-animation-fill-mode: forwards;
-  }
-
-  @media (max-width: 1000px) {
-    .itemExpanded {
-      margin-bottom: 10px;
-    }
-
-    .itemCollapsed {
-      margin-bottom: 40px;
-    }
-
-    .imageHolderLeft {
-      position: relative;
-      margin-left: auto;
-      margin-right: auto;
-      border-radius: 25px;
-      width: 400px;
-      height: 250px;
-      background-color: rgba(192,192,192, 0.4);
-      background-repeat: no-repeat;
-      background-blend-mode: screen;
-
-    }
-
-    .imageHolderRight {
-      position: relative;
-      margin-left: auto;
-      margin-right: auto;
-      border-radius: 25px;
-      width: 400px;
-      height: 250px;
-      background-color: rgba(192,192,192, 0.4);
-      background-repeat: no-repeat;
-      background-blend-mode: screen;
-    }
-
-    .infoLeft {
-      margin-right: 10vw;
-      margin-left: 10vw;
-      margin-top: 20px;
-      overflow: hidden;
-      text-align: center;
-    }
-
-    .infoRight {
-      margin-left: 10vw;
-      margin-right: 10vw;
-      margin-top: 20px;
-      overflow: hidden;
-      text-align: center;
-    }
-
-    .expandButtonUp {
-      position: relative;
-      margin-bottom: 25px;
-    }
-  }
-
-  @media (min-width: 1000px) {
-    .itemExpanded {
-      margin-bottom: 30px;
-    }
-
-    .itemCollapsed {
-      margin-bottom: 70px;
-    }
-
-    .imageHolderLeft {
-      float: left;
-      position: relative;
-      margin-left: 5vw;
-      margin-right: 20px;
-      border-radius: 25px;
-      width: 400px;
-      height: 250px;
-      background-color: rgba(192,192,192, 0.4);
-      background-repeat: no-repeat;
-      background-blend-mode: screen;
-    }
-
-    .imageHolderRight {
-      float: right;
-      position: relative;
-      margin-right: 5vw;
-      margin-left: 20px;
-      border-radius: 25px;
-      width: 400px;
-      height: 250px;
-      background-color: rgba(192,192,192, 0.4);
-      background-repeat: no-repeat;
-      background-blend-mode: screen;
-    }
-
-    .infoLeft {
-      margin-right: 5vw;
-      overflow: hidden;
-      text-align: left;
-    }
-
-    .infoRight {
-      margin-left: 5vw;
-      overflow: hidden;
-      text-align: left;
-    }
-  }
-
-  @media (max-width: 500px) {
-    .imageHolderLeft {
-      width: 350px;
-      height: 219px;
-    }
-
-    .imageHolderRight {
-      width: 350px;
-      height: 219px;
-    }
-
-    .image {
-      transform: scale(.85, .85) translate(-50%, -50%);
-      left: 42.5%;
-      top: 42.5%;
-    }
-
-    .expanded {
-      margin-bottom: 70px;
-    }
-
-    .expandButtonUp {
-      margin-top: -20vw;
     }
   }
 </style>
