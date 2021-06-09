b5bc22e440cbb38fbe98bae513c8bbc699fa81bb
George Sarantinos <georgesarantinos18@gmail.com>
2021-06-08 17:44:44 -0700

    send 404 if not cron

--- platform-service/routes/ping.js
+++ platform-service/routes/ping.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/keep-alive', async function(req, res, next) {
+  if (req.get('X-Appengine-Cron') !== 'true') {
+    res.sendStatus(404);
+    return;
+  }
+
   res.sendStatus(200);
 });
 
